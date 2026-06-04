05047a6cb4942a7beb0f9ed5a86825faf075b44b
ntlsnell <142305623+ntlsnell@users.noreply.github.com>
2026-06-04 18:24:50 +0300

    Create 02_jurisdiction_risk_profile.sql

---
+++ case-04-cross-dataset-analysis/sql/02_jurisdiction_risk_profile.sql
@@ -0,0 +1,28 @@
+-- JOIN Casino Market + AML Compliance datasets
+-- Shows market size vs regulatory risk per jurisdiction
+
+SELECT 
+    a.country,
+    a.region,
+    a.overall_aml_risk_score,
+    a.risk_category,
+    a.license_cost_usd,
+    COUNT(c.game) AS total_games,
+    COUNT(DISTINCT c.provider) AS total_providers,
+    ROUND(AVG(c.rtp), 2) AS avg_rtp
+FROM "igaming_aml_compliance_by_jurisdiction_clean" a
+LEFT JOIN (
+    SELECT game, provider, rtp,
+        CASE license_jurisdiction
+            WHEN 'MGA'       THEN 'Malta'
+            WHEN 'UKGC'      THEN 'United Kingdom'
+            WHEN 'Curaçao'   THEN 'Curacao'
+            WHEN 'ADM'       THEN 'Italy'
+            WHEN 'Kahnawake' THEN 'Canada'
+            ELSE license_jurisdiction
+        END AS mapped_country
+    FROM "online_casino_games_dataset_v2"
+) c ON a.country = c.mapped_country
+GROUP BY a.country, a.region, a.overall_aml_risk_score, 
+         a.risk_category, a.license_cost_usd
+ORDER BY total_games DESC;
